f435b2b5e8f0d3a81a344275d36cb40abc8809f7
Derek Grove <103955319+derekgrove@users.noreply.github.com>
2023-07-18 16:59:54 -0500

    Update
    Made some changes, now our files run once on a cluster, run that multiple times across all clusters

---
+++ center_charge.py
@@ -0,0 +1,16 @@
+def calculate_charge(cluster):
+    xs = [cluster[i] for i in range(0, len(cluster), 3)]
+    ys = [cluster[i] for i in range(1, len(cluster), 3)]
+    charges = [cluster[i] for i in range(2, len(cluster), 3)]
+
+    total_charge = sum(charges)
+
+    xcharge = sum(x * charge for x, charge in zip(xs, charges))
+    ycharge = sum(y * charge for y, charge in zip(ys, charges))
+
+    x_coc = xcharge / total_charge
+    y_coc = ycharge / total_charge
+
+    coc = (x_coc, y_coc, total_charge)
+
+    return coc
---
+++ plotter.py
@@ -0,0 +1,41 @@
+import numpy as np
+import matplotlib.colors as colors
+import matplotlib.pyplot as plt
+
+def plot_cluster(cluster):
+    xs = [cluster[i] for i in range(0, len(cluster), 3)]
+    ys = [cluster[i] for i in range(1, len(cluster), 3)]
+    charges = [cluster[i] for i in range(2, len(cluster), 3)]
+
+    xmin = min(xs) - 3
+    xmax = max(xs) + 3
+    ymin = min(ys) - 3
+    ymax = max(ys) + 3
+
+    
+    plt.hist2d(xs, ys, weights=charges, cmap='magma', bins=[range(256), range(256)])
+    plt.colorbar(label='Charge')
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.show()
+
+def plot_cluster_zoomed(cluster):
+    xs = [cluster[i] for i in range(0, len(cluster), 3)]
+    ys = [cluster[i] for i in range(1, len(cluster), 3)]
+    charges = [cluster[i] for i in range(2, len(cluster), 3)]
+
+    xmin = min(xs) - 3
+    xmax = max(xs) + 3
+    ymin = min(ys) - 3
+    ymax = max(ys) + 3
+
+    
+    histogram, xedges, yedges = np.histogram2d(xs, ys, weights=charges, bins=[range(xmin, xmax+1), range(ymin, ymax+1)])
+
+    plt.pcolormesh(xedges, yedges, histogram.T, norm=colors.LogNorm(), cmap='Purples')
+    plt.colorbar(label='Charge')
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.show()
+
+    
